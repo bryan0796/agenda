f0321a9ab0d6e344b174f123d083230346a93999
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-09-30 19:35:32 -0300

    first commit

---
+++ config/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'agenda';
+$user = 'root';
+$pass = "";
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+  // ativar modo de erros
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  $error = $e->getMessage();
+  echo "Erro: $error";
+}
