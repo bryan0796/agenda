b5799f4d4b5c783736206c0a415c8f74a45c049c
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-10-02 20:41:52 -0300

    ciracao da pagina show.php

--- config/process.php
+++ config/process.php
@@ -1,10 +1,26 @@
 <?php
 
-  SESSION_START();
+SESSION_START();
 
-  include_once("connection.php");
-  include_once("url.php");
+include_once("connection.php");
+include_once("url.php");
 
+$id;
+if (!empty($_GET)) {
+  $id = $_GET["id"];
+}
+
+// retorna um contato
+if (!empty($id)) {
+  $query = "SELECT * FROM contacts WHERE id = :id";
+
+  $stmt = $conn->prepare($query);
+  $stmt->bindParam(":id", $id);
+  $stmt->execute();
+
+  $contact = $stmt->fetch();
+} else {
+  // retorna todos os contatos
   $contacts = [];
   $query = "SELECT * FROM contacts";
 
@@ -12,3 +28,4 @@
   $stmt->execute();
 
   $contacts = $stmt->fetchAll();
+}
